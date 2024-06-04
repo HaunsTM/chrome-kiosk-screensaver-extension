e0136f3a85f7ff4c6140498888a8325902da5aa0
HaunsTM <hans.baldetorp@knowit.se>
2024-06-04 23:03:30 +0200

    Better refactoring

--- background.js
+++ background.js
@@ -7,12 +7,14 @@ const State = Object.freeze({
 
 const settings = {
   screensaverPage : {
-    url: 'https://www.svt.se/',
+    urlStart: 'http://10.0.0.6:8123/local/screensaver/index.html#/screen-saver',
+    regexMandatoryUrl: /(https?:[/]{2})10[.]0[.]0[.]6[:]8123+[/](?=.*screen-saver).*/g,
     dimPercent: 80   //fully dimmed
   },
   
   webPage : {
-    url: 'https://www.sydsvenskan.se/lund',
+    urlStart: 'http://10.0.0.6:8123/lovelace/default_view',
+    regexMandatoryUrl: /(https?:[/]{2})10[.]0[.]0[.]6[:]8123+[/](?!local[/]).*/g,
     dimPercent: 0   //no dim
   },
   noDim: 0,
@@ -29,17 +31,32 @@ const runtime = {
   },
   state : State.INITIAL,
   countdownBeforeIdlenessTimerId: null,
-  countdownCounterTimerId: null
+  countdownCounterTimerId: null,
+  countdownCounterValue: -1
 }
 
-function findTabId(url) {
+const WriteToLog = (functionName, reason, logMessage) => {
+  let currentDateTime = new Date();
+  let year = currentDateTime.getFullYear();
+  let month = String(currentDateTime.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed in JS
+  let date = String(currentDateTime.getDate()).padStart(2, '0');
+  let hours = String(currentDateTime.getHours()).padStart(2, '0');
+  let minutes = String(currentDateTime.getMinutes()).padStart(2, '0');
+  let seconds = String(currentDateTime.getSeconds()).padStart(2, '0');
+
+  console.log(`[${year}-${month}-${date} ${hours}:${minutes}:${seconds} ] [${functionName}] [${reason}] ${logMessage}`);
+}
+
+function findTabId(regex) {
   return new Promise((resolve, reject) => {
-    chrome.tabs.query({url: url}, (tabs) => {
-      if (tabs.length > 0) {
-        resolve(tabs[0].id);  // Return the ID of the first tab that matches the URL
-      } else {
-        resolve(-1);  // Return -1 if no tabs match the URL
+    chrome.tabs.query({}, (tabs) => {
+      for(let i = 0; i < tabs.length; i++) {
+        if (regex.test(tabs[i].url)) {
+          resolve(tabs[i].id);  // Return the ID of the first tab that matches the URL
+          return;
+        }
       }
+      resolve(-1);  // Return -1 if no tabs match the URL
     });
   });
 }
@@ -52,21 +69,25 @@ async function createTabAndGetId(url) {
   });
 }
 
-async function OpenWebPageTab(url) {
-  const existingTabId = await findTabId(url);
+async function OpenWebPageTab(targetUrl, targetUrlRegex) {
+  const existingTabId = await findTabId(targetUrlRegex);
   if (existingTabId !== -1) {
+    WriteToLog('OpenWebPageTab', `Alreday opened `, `${targetUrl}`);
     return existingTabId;
   } else {
-    const newTabId =  await createTabAndGetId(url);
+    const newTabId =  await createTabAndGetId(targetUrl);
+    WriteToLog('OpenWebPageTab', `Not opened`, `${targetUrl}`);
     return newTabId;
   }
 }
 
-async function OpenWebPageTabs() {
-  const screensaverPageTabPromise = OpenWebPageTab(settings.screensaverPage.url);
-  const webPageTabPromise = OpenWebPageTab(settings.webPage.url);
+async function EnsureOpenWebPagesAndUpdateTabIds() {
+  const screensaverPageTabPromise = OpenWebPageTab(settings.screensaverPage.urlStart, settings.screensaverPage.regexMandatoryUrl);
+  const webPageTabPromise = OpenWebPageTab(settings.webPage.urlStart, settings.webPage.regexMandatoryUrl);
 
   [runtime.screensaver.tabId, runtime.webPage.tabId] = await Promise.all([screensaverPageTabPromise, webPageTabPromise]);
+  WriteToLog('OpenWebPageTabs', 'runtime.screensaver.tabId: ', `${runtime.screensaver.tabId}`);
+  WriteToLog('OpenWebPageTabs', 'runtime.webPage.tabId: ', `${runtime.webPage.tabId}`);
 }
 
 function ChangeScreenBrightness(tabId, dimPercent) {
@@ -79,9 +100,45 @@ function ChangeScreenBrightness(tabId, dimPercent) {
         dimPercent: dimPercent
       }, function(response) {
         if (response) {
-          console.log(response.status);  // Logs 'success'
+          WriteToLog('ChangeScreenBrightness', 'change_screen_brightness done: ', response.status);
         } else {
-          console.log('No response received');
+          WriteToLog('ChangeScreenBrightness', 'change_screen_brightness failed: ', 'No response received');
+        }
+      });
+    }
+  });
+};
+
+
+function PrintCountDownCounterValue(tabId, counterValue) {
+  chrome.tabs.get(tabId, function(tab) {
+    if (chrome.runtime.lastError) {
+      console.log(chrome.runtime.lastError.message);
+    } else if (tab.status === 'complete') {
+      chrome.tabs.sendMessage(tabId, {
+        message: 'countdown',
+        counterValue: counterValue
+      }, function(response) {
+        if (response) {
+          WriteToLog('PrintCountDownCounterValue', 'countdown performed: ', response.status);
+        } else {
+          WriteToLog('PrintCountDownCounterValue', 'countdown failed: ', 'No response received');
+        }
+      });
+    }
+  });
+};
+
+function RemoveCountDownCounter(tabId) {
+  chrome.tabs.get(tabId, function(tab) {
+    if (tab.status === 'complete') {
+      chrome.tabs.sendMessage(tabId, {
+        message: 'countdownRemove',
+      }, function(response) {
+        if (response) {
+          WriteToLog('RemoveCountDownCounter', 'countdownRemove performed: ', response.status);
+        } else {
+          WriteToLog('RemoveCountDownCounter', 'countdownRemove failed: ', 'No response received');
         }
       });
     }
@@ -91,50 +148,59 @@ function ChangeScreenBrightness(tabId, dimPercent) {
 chrome.idle.onStateChanged.addListener(async (state) => {
   if (state === 'idle') {
     runtime.state = State.IDLE;
-    // Clear the countdown if the state changes to 'idle'
-    if (runtime.countdownBeforeIdlenessTimerId) {
-      clearTimeout(runtime.countdownBeforeIdlenessTimerId);
-      runtime.countdownBeforeIdlenessTimerId = null;
-    }
   } else if (state === 'active') {
     runtime.state = State.ACTIVE;
-    // Start a countdown when the state changes to 'active'
-    runtime.countdownBeforeIdlenessTimerId = setTimeout(() => {
-      console.log('5 seconds left before idle');
-    }, (settings.idleTime - 5) * 1000);
-  }  
+  }
+  ChangeState();
 });
 
-
-async function ChangeState() {
-
-  await OpenWebPageTabs();
-
-  if (runtime.state === State.INITIAL) {
-
-    runtime.state = State.ACTIVE;    
-    //chrome.idle.setDetectionInterval(settings.idleTime);
-    setInterval(async function() { await ChangeState(); }, 5000);
-
-  } else if (runtime.state === State.ACTIVE) {
-    await OpenWebPageTabs();
-    chrome.tabs.update(runtime.webPage.tabId, {active: true});
-    ChangeScreenBrightness(runtime.screensaver.tabId, settings.noDim);
-    runtime.state = State.IDLE;
-
-  } else if (runtime.state === State.COUNTDOWN_COUNTER) {
-    chrome.tabs.update(runtime.screensaver.tabId, {active: true});
-    ChangeScreenBrightness(runtime.screensaver.tabId, settings.screensaverPage.dimPercent);
-    await OpenWebPageTabs();
-    runtime.state = State.ACTIVE;
-  } else if (runtime.state === State.IDLE) {
-    chrome.tabs.update(runtime.screensaver.tabId, {active: true});
-    ChangeScreenBrightness(runtime.screensaver.tabId, settings.screensaverPage.dimPercent);
-    await OpenWebPageTabs();
-    runtime.state = State.ACTIVE;
+function StopAndResetTimerSafe(timerId) {
+  if (timerId) {
+    clearTimeout(timerId);
   }
 }
 
+async function ChangeState() {
+
+  await EnsureOpenWebPagesAndUpdateTabIds();
+  StopAndResetTimerSafe(runtime.countdownBeforeIdlenessTimerId);
+  StopAndResetTimerSafe(runtime.countdownCounterTimerId);
+  
+  if (runtime.state === State.INITIAL) {
+    runtime.state = State.ACTIVE;
+    chrome.idle.setDetectionInterval(settings.idleTime);
+
+  } else if (runtime.state === State.ACTIVE) {
+
+    chrome.tabs.update(runtime.webPage.tabId, {active: true});
+    ChangeScreenBrightness(runtime.screensaver.tabId, settings.noDim);
+    
+    runtime.countdownBeforeIdlenessTimerId = setTimeout(() => {
+      runtime.state = State.COUNTDOWN_COUNTER;
+      StopAndResetTimerSafe(runtime.countdownBeforeIdlenessTimerId);
+      ChangeState();
+    }, (settings.idleTime - 5) * 1000);
+    
+  } else if (runtime.state === State.COUNTDOWN_COUNTER) {
+
+    runtime.countdownCounterValue = 5; // Set the initial count
+
+
+    runtime.countdownCounterTimerId = setInterval(() => {
+      runtime.countdownCounterValue--;
+        PrintCountDownCounterValue(runtime.webPage.tabId, runtime.countdownCounterValue);
+        if (runtime.countdownCounterValue <= 0) {          
+          StopAndResetTimerSafe(runtime.countdownCounterTimerId);
+        }
+    }, 1000); // 1000 milliseconds = 1 second      
+
+  } else if (runtime.state === State.IDLE) {
+    chrome.tabs.update(runtime.screensaver.tabId, {active: true});
+    
+    RemoveCountDownCounter(runtime.webPage.tabId);
+    ChangeScreenBrightness(runtime.screensaver.tabId, settings.screensaverPage.dimPercent);
+  }
+}
 
 // start the extension
 (async function() {
--- content.js
+++ content.js
@@ -1,10 +1,12 @@
+const dimOverlay = 'dimOverlay';
+
 const PerformDimScreen = (dimValue) => {
-  let overlay = document.getElementById('dimOverlay');
+  let overlay = document.getElementById(dimOverlay);
   
   // If the overlay doesn't exist, create it
   if (!overlay) {
     overlay = document.createElement('div');
-    overlay.id = 'dimOverlay';
+    overlay.id = dimOverlay;
     overlay.style.position = 'fixed';
     overlay.style.top = 0;
     overlay.style.left = 0;
@@ -18,7 +20,7 @@ const PerformDimScreen = (dimValue) => {
 }
 
 const PerformUndimScreen = () => {
-  let overlay = document.getElementById('dimOverlay');
+  let overlay = document.getElementById(dimOverlay);
   
   // If the overlay exists, remove it
   if (overlay) {
@@ -26,21 +28,15 @@ const PerformUndimScreen = () => {
   }
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(request);
-  if (request.message === 'change_screen_brightness') {
-    const dimValue = request.dimPercent / 100;
-    if (dimValue > 0) {
-      PerformDimScreen(dimValue);
-      sendResponse({status: 'dimmed screen'});  // Send a response back to the sender
-    } else {
-      PerformUndimScreen();
-      sendResponse({status: 'undimmed screen'});  // Send a response back to the sender
-    }
-  }
-  else if (request.message === 'start_countdown') {
-    let countdown = request.countdown;
-    let countdownElement = document.createElement('div');
+const dimCountdownElement = 'dimCountdownElement';
+
+const PrintCounterValue = (counterValue) => {
+  let countdownElement = document.getElementById(dimCountdownElement);
+  
+  // If the overlay doesn't exist, create it
+  if (!countdownElement) {
+    countdownElement = document.createElement('div');
+    countdownElement.id = dimCountdownElement;
     countdownElement.style.position = 'fixed';
     countdownElement.style.bottom = '20px';
     countdownElement.style.right = '20px';
@@ -50,18 +46,40 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     countdownElement.style.padding = '10px';
     countdownElement.style.borderRadius = '5px';
     document.body.appendChild(countdownElement);
+  }
+  
+  countdownElement.textContent = counterValue;
+}
 
-    let countdownInterval = setInterval(() => {
-      countdownElement.textContent = countdown;
-      countdown--;
+const RemoveCounterValue = () => {
+  let countdownElement = document.getElementById(dimCountdownElement);
+  
+  // If the element exists, remove it
+  if (countdownElement) {
+    countdownElement.remove();
+  }
+}
 
-      if (countdown < 0) {
-        clearInterval(countdownInterval);
-        document.body.removeChild(countdownElement);
-      }
-    }, 1000);
-    
-    sendResponse({status: 'count down started'});  // Send a response back to the sender
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  
+  if (request.message === 'change_screen_brightness') {
+    const dimValue = request.dimPercent / 100;
+    if (dimValue > 0) {
+      PerformDimScreen(dimValue);
+      sendResponse({status: `dimmed screen to ${dimValue} performed`});  // Send a response back to the sender
+    } else {
+      PerformUndimScreen();
+      sendResponse({status: `undimmed screen performed`});  // Send a response back to the sender
+    }
+  }
+  else if (request.message === 'countdown') {
+    let countdown = request.counterValue;
+    PrintCounterValue(countdown);
+    sendResponse({status: `count down ${countdown} performed`});  // Send a response back to the sender
+  }
+  else if (request.message === 'countdownRemove') {
+    RemoveCounterValue();
+    sendResponse({status: `count down removed performed`});  // Send a response back to the sender
   }
   return true;
 });
